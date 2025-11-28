077c2394162d8f25b32d4bf1ecf23f93051e31b2
joep van opdorp <joep@home.homepc.nl>
2025-11-28 15:18:41 +0100

    Removing file

--- build/output.cpp
+++
@@ -1,9 +0,0 @@
-
-    #include "/home/joep/threadon/v2/source_code/lib/sttlib/sttlib.hpp"
-    using namespace std;
-    void thread_starter() {
-}
-int main() {thread_starter();
-print(helllo);
-x = 4;
-}
